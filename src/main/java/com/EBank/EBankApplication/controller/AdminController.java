b106f2597c223e1e2b9b49091dfe0bc1dc830553
deyki <deikioveca@gmail.com>
2023-06-05 19:58:55 +0300

    banBankAccountByIban created

--- src/main/java/com/EBank/EBankApplication/controller/AdminController.java
+++ src/main/java/com/EBank/EBankApplication/controller/AdminController.java
@@ -1,15 +1,34 @@
 package com.EBank.EBankApplication.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.EBank.EBankApplication.error.BankAccountNotFoundException;
+import com.EBank.EBankApplication.model.ResponseMessage;
+import com.EBank.EBankApplication.model.bankAccount.BankAccountRequest;
+import com.EBank.EBankApplication.service.impl.AdminServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    private final AdminServiceImpl adminService;
+
+    @Autowired
+    public AdminController(AdminServiceImpl adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping
     public String adminPanel() {
         return "Admin panel!";
+    }
+
+    @PostMapping("/bankAccount-ban")
+    public ResponseEntity<ResponseMessage> banBankAccountByIban(@RequestBody BankAccountRequest bankAccountRequest) throws BankAccountNotFoundException {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(adminService.banBankAccountByIban(bankAccountRequest));
     }
 }
